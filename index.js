750dcd11f1e49e7da124631edf3061490697b3ba
jacksos101 <jacksos101@gmail.com>
2022-10-24 17:24:28 +1300

    Update index.js
    Modified response message.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Modified product feed goes here.')
 })
 app.listen(process.env.PORT || 3000)
